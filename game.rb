bd2821b6d2b5c421e53de244c522711eaf8d4e23
megan <meganambermcbride@gmail.com>
2022-07-25 13:10:23 -0400

    Add change player method

--- game.rb
+++ game.rb
@@ -6,12 +6,21 @@ class Game
     @current_player = players[0]
     @player1 = player1
     @player2 = player2
-  end 
+  end  
+
   def lost(player)
     if player == @player1.name
       @player1.score = @player1.score - 1
     elsif player == @player2.name
       @player2.score = @player2.score - 1
+    end
+  end
+
+  def change_player
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    elsif
+      @current_player = @players[0]
     end
   end
 
