441c2663d5f5f4250eec2339a175bf3da854639e
megan <meganambermcbride@gmail.com>
2022-07-25 13:11:28 -0400

    Add game flow logic

--- main.rb
+++ main.rb
@@ -7,3 +7,33 @@ player1 = Player.new("player1")
 player2 = Player.new("player2")
 game = Game.new(player1, player2)
 
+while player1.score > 0 && player2.score > 0
+
+  puts question.generate_question(game.current_player)
+
+  input = gets.chomp.to_i
+
+  if input == question.answer
+    puts " #{game.current_player} : Correct answer!"
+  elsif
+    game.lost(game.current_player)
+    puts " #{game.current_player} : Incorrect answer - 1 "
+  end
+
+  puts " P1: #{player1.score}/3 vs P2: #{player2.score}/3"
+
+  puts "---------- NEW TURN ----------"
+
+  game.change_player
+
+  if player1.score == 0 
+    puts "Player 2 wins with a score of #{player2.score}/3"
+    puts "---------- GAME OVER ----------"
+    puts "Good bye!"
+  elsif player2.score == 0
+    puts "Player 1 wins with a score of #{player1.score}/3"
+    puts "---------- GAME OVER ----------"
+    puts "Good bye!"
+  end
+
+end
